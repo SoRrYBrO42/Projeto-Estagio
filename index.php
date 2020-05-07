e63b0c15d0aa6ce9f6b3c19cc415494c4aa182eb
Diogo1080 <51164185+Diogo1080@users.noreply.github.com>
2020-05-07 14:42:50 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
-<?php ?>
+<?php 
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Clube estrela azul</title>
 </head>
 <body>
-
+	<div>
+		<form>
+			<label>Username:</label>
+				<input name="username"><br>
+			<label>Password:</label>
+				<input type="Password" name="password"><br>
+		</form>
+	</div>
 </body>
 </html>
