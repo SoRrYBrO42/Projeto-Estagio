5f103bce9596c352235ccbfe910410d9a3dc0518
Diogo1080 <51164185+Diogo1080@users.noreply.github.com>
2020-05-19 17:49:54 +0100

    Update recursos_humanos.php

--- recursos_humanos.php
+++ recursos_humanos.php
@@ -1,122 +1,122 @@
 <?php 
 	require ('ligacao.php');
-	if (isset($_POST['insert']) or isset($_POST['update'])) {
-		# code...
-	}
 	if (isset($_POST['insert'])) {
 		$querry=$con->prepare("INSERT INTO `recursos_humanos`(`nome`, `sexo`, `dt_nasc`, `morada`, `localidade`, `freguesia`, `concelho`, `CP`, `email`, `telemovel`, `CC`, `NIF`, `registo_criminal`, `certificado_academico`, `certificado_sbv_dae`, `certificado_direcao`, `salario`, `foto`) 
 			VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
 
-		//foto
-		if (!isset($_FILES["foto"]["name"])){
-			if ($sexo=="Masculino") {
-				$foto="Male_user.png";
-			}else{
-				$foto="Female_user.png";
-			}
-		}else{
-			$foto_original=$_FILES["foto"]["name"];
-			$extencao = explode(".", $foto_original);
-			$foto=$_POST['cc'].".".end($extencao);
-			move_uploaded_file($_FILES["foto"]["tmp_name"], "fotos/" . $foto);
-		}
+		//ficheiros
+			//foto
+				if (!isset($_FILES["foto"]["name"])){
+					if ($sexo=="Masculino") {
+						$foto="Male_user.png";
+					}else{
+						$foto="Female_user.png";
+					}
+				}else{
+					$foto_original=$_FILES["foto"]["name"];
+					$extencao = explode(".", $foto_original);
+					$foto=$_POST['cc'].".".end($extencao);
+					move_uploaded_file($_FILES["foto"]["tmp_name"], "fotos/" . $foto);
+				}
 
-		//registo criminal
-		if (!isset($_FILES["registo_criminal"]["name"])) {
-			$registo_criminal="";
-		}else{
-			$ficheiro=$_FILES["registo_criminal"]["name"];
-			$extencao=explode(".", $ficheiro);
-			$registo_criminal=$_POST['cc'].".".end($extencao);
+			//registo criminal
+				if (!isset($_FILES["registo_criminal"]["name"])) {
+					$registo_criminal="";
+				}else{
+					$ficheiro=$_FILES["registo_criminal"]["name"];
+					$extencao=explode(".", $ficheiro);
+					$registo_criminal=$_POST['cc'].".".end($extencao);
 
-			$targetfolder = "ficheiros/registos_criminais/".basename($registo_criminal);
+					$targetfolder = "ficheiros/registos_criminais/".basename($registo_criminal);
 
-			if(move_uploaded_file($_FILES['registo_criminal']['tmp_name'], $targetfolder)){
-				echo "The file ". basename( $_FILES['registo_criminal']['name']). " is uploaded";
-			}else{
-				echo "Problem uploading file";
-			}
-		}
+					if(move_uploaded_file($_FILES['registo_criminal']['tmp_name'], $targetfolder)){
+						echo "The file ". basename( $_FILES['registo_criminal']['name']). " is uploaded";
+					}else{
+						echo "Problem uploading file";
+					}
+				}
 
-		//certificado academico
-		if (!isset($_FILES["certificado_academico"]["name"])) {
-			$certificado_academico="";
-		}else{
-			$ficheiro=$_FILES["certificado_academico"]["name"];
-			$extencao=explode(".", $ficheiro);
-			$certificado_academico=$_POST['cc'].".".end($extencao);
+			//certificado academico
+				if (!isset($_FILES["certificado_academico"]["name"])) {
+					$certificado_academico="";
+				}else{
+					$ficheiro=$_FILES["certificado_academico"]["name"];
+					$extencao=explode(".", $ficheiro);
+					$certificado_academico=$_POST['cc'].".".end($extencao);
 
-			$targetfolder = "ficheiros/certificados_academicos/".basename($certificados_academico) ;
+					$targetfolder = "ficheiros/certificados_academicos/".basename($certificados_academico) ;
 
-			if(move_uploaded_file($_FILES['certificado_academico']['tmp_name'], $targetfolder)){
-				echo "The file ". basename( $_FILES['certificado_academico']['name']). " is uploaded";
-			}else{
-				echo "Problem uploading file";
-			}
-		}
+					if(move_uploaded_file($_FILES['certificado_academico']['tmp_name'], $targetfolder)){
+						echo "The file ". basename( $_FILES['certificado_academico']['name']). " is uploaded";
+					}else{
+						echo "Problem uploading file";
+					}
+				}
 
-		//certificado academico
-		if (!isset($_FILES["certificado_sbv_dae"]["name"])) {
-			$certificado_sbv_dae="";
-		}else{
-			$ficheiro=$_FILES["certificado_sbv_dae"]["name"];
-			$extencao=explode(".", $ficheiro);
-			$certificado_sbv_dae=$_POST['cc'].".".end($extencao);
+			//certificado sbv e dae
+				if (!isset($_FILES["certificado_sbv_dae"]["name"])) {
+					$certificado_sbv_dae="";
+				}else{
+					$ficheiro=$_FILES["certificado_sbv_dae"]["name"];
+					$extencao=explode(".", $ficheiro);
+					$certificado_sbv_dae=$_POST['cc'].".".end($extencao);
 
-			$targetfolder = "ficheiros/certificado_sbv_dae/".basename($certificado_sbv_dae) ;
+					$targetfolder = "ficheiros/certificado_sbv_dae/".basename($certificado_sbv_dae) ;
 
-			if(move_uploaded_file($_FILES['certificado_sbv_dae']['tmp_name'], $targetfolder)){
-				echo "The file ". basename( $_FILES['certificado_sbv_dae']['name']). " is uploaded";
-			}else{
-				echo "Problem uploading file";
-			}
-		}
+					if(move_uploaded_file($_FILES['certificados_sbv_dae']['tmp_name'], $targetfolder)){
+						echo "The file ". basename( $_FILES['certificado_sbv_dae']['name']). " is uploaded";
+					}else{
+						echo "Problem uploading file";
+					}
+				}	
 
-		//certificado academico
-		if (!isset($_FILES["certificado_sbv_dae"]["name"])) {
-			$certificado_sbv_dae="";
-		}else{
-			$ficheiro=$_FILES["certificado_sbv_dae"]["name"];
-			$extencao=explode(".", $ficheiro);
-			$certificado_sbv_dae=$_POST['cc'].".".end($extencao);
+			//certificado desportivo
+				if (!isset($_FILES["certificado_desportivo"]["name"])) {
+					$certificado_desportivo="";
+				}else{
+					$ficheiro=$_FILES["certificado_desportivo"]["name"];
+					$extencao=explode(".", $ficheiro);
+					$certificado_desportivo=$_POST['cc'].".".end($extencao);
 
-			$targetfolder = "ficheiros/certificado_sbv_dae/".basename($certificado_sbv_dae) ;
+					$targetfolder = "ficheiros/certificado_desportivo/".basename($certificado_desportivo) ;
 
-			if(move_uploaded_file($_FILES['certificado_sbv_dae']['tmp_name'], $targetfolder)){
-				echo "The file ". basename( $_FILES['certificado_sbv_dae']['name']). " is uploaded";
-			}else{
-				echo "Problem uploading file";
-			}
-		}
-		
-
-		//certificado desportivo
-		if (!isset($_FILES["certificado_desportivo"]["name"])) {
-			$certificado_desportivo="";
-		}else{
-			$ficheiro=$_FILES["certificado_desportivo"]["name"];
-			$extencao=explode(".", $ficheiro);
-			$certificado_desportivo=$_POST['cc'].".".end($extencao);
-
-			$targetfolder = "ficheiros/certificado_desportivo/".basename($certificado_desportivo) ;
-
-			if(move_uploaded_file($_FILES['certificado_desportivo']['tmp_name'], $targetfolder)){
-				echo "The file ". basename( $_FILES['certificado_desportivo']['name']). " is uploaded";
-			}else{
-				echo "Problem uploading file";
-			}
-		}
+					if(move_uploaded_file($_FILES['certificado_desportivo']['tmp_name'], $targetfolder)){
+						echo "The file ". basename( $_FILES['certificado_desportivo']['name']). " is uploaded";
+					}else{
+						echo "Problem uploading file";
+					}
+				}
 
 		$querry->bind_param("issssssisiiissssis",$_POST['nome'],$_POST['sexo'],$_POST['dt_nasc'],$_POST['morada'],$_POST['localidade'],$_POST['freguesia'],$_POST['concelho'],$_POST['cp'],$_POST['email'],$_POST['telemovel'],$_POST['cc'],$_POST['nif'],$registo_criminal,$certificado_academicot,$certificado_sbv_dae,$certificado_direcao,$_POST['salario'],$foto);
+
 		$querry->execute();
+
+		if ($querry->affected_rows===0) {
+			?>
+				<script type="text/javascript">
+					alert("Ocurreu algo não esperado.");
+				</script>
+			<?php
+		}else{
+			?>
+				<script type="text/javascript">
+					alert($_POST['nome']" inserido com sucesso.");
+				</script>
+			<?php	
+		}
 	}
+
 	if (isset($_POST['update'])) {
-		$querry=$con->prepare("UPDATE `recursos_humanos` SET `nome`=[value-3],`sexo`=[value-4],`dt_nasc`=[value-5],`morada`=[value-6],`localidade`=[value-7],`freguesia`=[value-8],`concelho`=[value-9],`CP`=[value-10],`email`=[value-11],`telemovel`=[value-12],`CC`=[value-13],`NIF`=[value-14],`registo_criminal`=[value-15],`certificado_academico`=[value-16],`certificado_sbv_dae`=[value-17],`certificado_direcao`=[value-18],`salario`=[value-19],`foto`=[value-20] WHERE `id_recurso_humano`=[value-1]");
+		$querry=$con->prepare("UPDATE `recursos_humanos` SET `nome`=?`sexo`=?`dt_nasc`=?`morada`=?`localidade`=?`freguesia`=?`concelho`=?`CP`=?,`email`=?,`telemovel`=?,`CC`=?,`NIF`=?,`registo_criminal`=?,`certificado_academico`=?,`certificado_sbv_dae`=?,`certificado_direcao`=?,`salario`=?,`foto`=? WHERE `id_recurso_humano`=?");
+
 	}
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+		<script src="//code.jquery.com/jquery.min.js"></script>
+		<script src="toastr/toastr.js"></script>
 		<title>Recursos humanos</title>
 	</head>
 	<body>
@@ -141,26 +141,37 @@
 							echo"Male_user.png";
 						} 
 					?>" alt="Foto do treinador" height="200" width="200"><br>
-				<label>Escolher a foto</label><br>
-	    			<input type="file" name="foto" accept="image/png, image/jpeg"><br>
-
+				<label>Escolher a foto</label>
+	    			<input type="file" id="foto" name="foto" accept="image/png, image/jpeg"><br>
 				<label>Cargos:</label><br>
-				<label>
 					<?php 
 						$querry = $con->prepare("SELECT * FROM cargos");
 						$querry->execute();
 						$resultado=$querry->get_result();
 						if($resultado->num_rows === 0){ 
-							echo "";
+							echo "Não existem cargos disponiveis.";
 						}else{
-							while ($linha=$resultado->fetch_assoc()) {
-								?>
-									<?php echo($linha['cargo']); ?><input type="checkbox" id="<?php echo($linha['id_cargo']); ?>" name="cargo[]">
-								<?php
+							while ($linha=$resultado->fetch_assoc()) { ?>
+								<label>
+									<?php 
+										echo($linha['cargo']);
+										if (strpos($linha['cargo'],'Treinador')!==false) {
+											?>
+												<input onclick="alert('função que faz aparecer os campos do treinador.');" type="checkbox" id="<?php echo($linha['id_cargo']); ?>" name="cargo[]">
+											<?php
+										}else{
+											?>
+												<input type="checkbox" id="<?php echo($linha['id_cargo']); ?>" name="cargo[]">
+
+											<?php
+										}
+									?>
+								</label>
+								<br>
+							<?php
 							}	
 						}
 					?>
-				</label><br>
 				<label>Salario:</label>
 					<input name="salario">€<br>
 				<label>Nome:</label>
@@ -182,7 +193,7 @@
 				<label>Telemovel:</label>
 					<input name="telemovel"><br>
 				<label>Sexo:</label>
-					<select>
+					<select id="sexo" name="sexo" onchange="mudar_imagem()">
 						<option value="Masculino">Masculino</option>
 						<option value="Feminino">Feminino</option>
 					</select><br>
@@ -191,14 +202,49 @@
 				<label>Registo criminal:</label>
 					<input type="file" name="registo_criminal" accept=".pdf,.doc"><br>
 				<label>Certificado academico:</label>
-					<input type="file" name="certificado_academico"><br>
+					<input type="file" name="certificado_academico" accept=".pdf,.doc"><br>
 				<label>Certificado sbv/dae:</label>
-					<input type="file" name="certificado_sbv_dae"><br>
+					<input type="file" name="certificado_sbv_dae" accept=".pdf,.doc"><br>
 				<input type="submit" name="insert" value="Inserir">
 			</form>
 		</div>
 	</body>
 </html>
+<!--Faz upload da foto para mostrar no site-->
+<script type="text/javascript">
+	function readURL(input) {
+		if (input.files && input.files[0]) {
+			var reader = new FileReader();
+
+			reader.onload = function(e) {
+				$('#foto_place').attr('src', e.target.result);
+			}
+			reader.readAsDataURL(input.files[0]);
+		}
+	}
+
+	$("#foto").change(function() {
+		readURL(this);
+	});
+</script>
+<?php
+	if (!isset($_GET['id_recurso_humano'])) {
+		?>
+		<script>
+			//Função de escolher a imagem consuante o sexo
+			function mudar_imagem(){
+				if ((document.getElementById("foto").value=='')) {
+					if (document.getElementById('sexo').value=="Masculino") {
+						document.getElementById('foto_place').src="fotos/Male_user.png"
+					}else{
+						document.getElementById('foto_place').src="fotos/Female_user.png"
+					}
+				}
+			}
+		</script>
+		<?php
+	}
+?>
 <script type="text/javascript">
 
 </script>
