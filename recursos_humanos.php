00548db369ea874f044f3d45f6a2a0d543096c70
Diogo1080 <51164185+Diogo1080@users.noreply.github.com>
2020-05-19 16:49:49 +0100

    Update recursos_humanos.php

--- recursos_humanos.php
+++ recursos_humanos.php
@@ -1,5 +1,118 @@
 <?php 
 	require ('ligacao.php');
+	if (isset($_POST['insert']) or isset($_POST['update'])) {
+		# code...
+	}
+	if (isset($_POST['insert'])) {
+		$querry=$con->prepare("INSERT INTO `recursos_humanos`(`nome`, `sexo`, `dt_nasc`, `morada`, `localidade`, `freguesia`, `concelho`, `CP`, `email`, `telemovel`, `CC`, `NIF`, `registo_criminal`, `certificado_academico`, `certificado_sbv_dae`, `certificado_direcao`, `salario`, `foto`) 
+			VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
+
+		//foto
+		if (!isset($_FILES["foto"]["name"])){
+			if ($sexo=="Masculino") {
+				$foto="Male_user.png";
+			}else{
+				$foto="Female_user.png";
+			}
+		}else{
+			$foto_original=$_FILES["foto"]["name"];
+			$extencao = explode(".", $foto_original);
+			$foto=$_POST['cc'].".".end($extencao);
+			move_uploaded_file($_FILES["foto"]["tmp_name"], "fotos/" . $foto);
+		}
+
+		//registo criminal
+		if (!isset($_FILES["registo_criminal"]["name"])) {
+			$registo_criminal="";
+		}else{
+			$ficheiro=$_FILES["registo_criminal"]["name"];
+			$extencao=explode(".", $ficheiro);
+			$registo_criminal=$_POST['cc'].".".end($extencao);
+
+			$targetfolder = "ficheiros/registos_criminais/".basename($registo_criminal);
+
+			if(move_uploaded_file($_FILES['registo_criminal']['tmp_name'], $targetfolder)){
+				echo "The file ". basename( $_FILES['registo_criminal']['name']). " is uploaded";
+			}else{
+				echo "Problem uploading file";
+			}
+		}
+
+		//certificado academico
+		if (!isset($_FILES["certificado_academico"]["name"])) {
+			$certificado_academico="";
+		}else{
+			$ficheiro=$_FILES["certificado_academico"]["name"];
+			$extencao=explode(".", $ficheiro);
+			$certificado_academico=$_POST['cc'].".".end($extencao);
+
+			$targetfolder = "ficheiros/certificados_academicos/".basename($certificados_academico) ;
+
+			if(move_uploaded_file($_FILES['certificado_academico']['tmp_name'], $targetfolder)){
+				echo "The file ". basename( $_FILES['certificado_academico']['name']). " is uploaded";
+			}else{
+				echo "Problem uploading file";
+			}
+		}
+
+		//certificado academico
+		if (!isset($_FILES["certificado_sbv_dae"]["name"])) {
+			$certificado_sbv_dae="";
+		}else{
+			$ficheiro=$_FILES["certificado_sbv_dae"]["name"];
+			$extencao=explode(".", $ficheiro);
+			$certificado_sbv_dae=$_POST['cc'].".".end($extencao);
+
+			$targetfolder = "ficheiros/certificado_sbv_dae/".basename($certificado_sbv_dae) ;
+
+			if(move_uploaded_file($_FILES['certificado_sbv_dae']['tmp_name'], $targetfolder)){
+				echo "The file ". basename( $_FILES['certificado_sbv_dae']['name']). " is uploaded";
+			}else{
+				echo "Problem uploading file";
+			}
+		}
+
+		//certificado academico
+		if (!isset($_FILES["certificado_sbv_dae"]["name"])) {
+			$certificado_sbv_dae="";
+		}else{
+			$ficheiro=$_FILES["certificado_sbv_dae"]["name"];
+			$extencao=explode(".", $ficheiro);
+			$certificado_sbv_dae=$_POST['cc'].".".end($extencao);
+
+			$targetfolder = "ficheiros/certificado_sbv_dae/".basename($certificado_sbv_dae) ;
+
+			if(move_uploaded_file($_FILES['certificado_sbv_dae']['tmp_name'], $targetfolder)){
+				echo "The file ". basename( $_FILES['certificado_sbv_dae']['name']). " is uploaded";
+			}else{
+				echo "Problem uploading file";
+			}
+		}
+		
+
+		//certificado desportivo
+		if (!isset($_FILES["certificado_desportivo"]["name"])) {
+			$certificado_desportivo="";
+		}else{
+			$ficheiro=$_FILES["certificado_desportivo"]["name"];
+			$extencao=explode(".", $ficheiro);
+			$certificado_desportivo=$_POST['cc'].".".end($extencao);
+
+			$targetfolder = "ficheiros/certificado_desportivo/".basename($certificado_desportivo) ;
+
+			if(move_uploaded_file($_FILES['certificado_desportivo']['tmp_name'], $targetfolder)){
+				echo "The file ". basename( $_FILES['certificado_desportivo']['name']). " is uploaded";
+			}else{
+				echo "Problem uploading file";
+			}
+		}
+
+		$querry->bind_param("issssssisiiissssis",$_POST['nome'],$_POST['sexo'],$_POST['dt_nasc'],$_POST['morada'],$_POST['localidade'],$_POST['freguesia'],$_POST['concelho'],$_POST['cp'],$_POST['email'],$_POST['telemovel'],$_POST['cc'],$_POST['nif'],$registo_criminal,$certificado_academicot,$certificado_sbv_dae,$certificado_direcao,$_POST['salario'],$foto);
+		$querry->execute();
+	}
+	if (isset($_POST['update'])) {
+		$querry=$con->prepare("UPDATE `recursos_humanos` SET `nome`=[value-3],`sexo`=[value-4],`dt_nasc`=[value-5],`morada`=[value-6],`localidade`=[value-7],`freguesia`=[value-8],`concelho`=[value-9],`CP`=[value-10],`email`=[value-11],`telemovel`=[value-12],`CC`=[value-13],`NIF`=[value-14],`registo_criminal`=[value-15],`certificado_academico`=[value-16],`certificado_sbv_dae`=[value-17],`certificado_direcao`=[value-18],`salario`=[value-19],`foto`=[value-20] WHERE `id_recurso_humano`=[value-1]");
+	}
 ?>
 <!DOCTYPE html>
 <html>
@@ -9,7 +122,7 @@
 	<body>
 		<?php require ('nav.php'); ?>
 		<div>
-			<form method="POST">
+			<form method="POST" enctype="multipart/form-data">
 				<img id="foto_place" src="fotos/
 					<?php 
 						if (isset($_GET['id_treinador'])){
@@ -31,25 +144,23 @@
 				<label>Escolher a foto</label><br>
 	    			<input type="file" name="foto" accept="image/png, image/jpeg"><br>
 
-				<label>Cargo:</label>
-					<select>
-						<?php 
-							$querry = $con->prepare("SELECT * FROM cargos");
-							$querry->execute();
-							$resultado=$querry->get_result();
-							if($resultado->num_rows === 0){ 
-								echo "";
-							}else{
-								while ($linha=$resultado->fetch_assoc()) {
-									?>
-										<option id="<?php echo($linha['id_cargo']); ?>">
-											<?php echo($linha['cargo']); ?>
-										</option>
-									<?php
-								}
-							};
-						?>
-					</select><br>
+				<label>Cargos:</label><br>
+				<label>
+					<?php 
+						$querry = $con->prepare("SELECT * FROM cargos");
+						$querry->execute();
+						$resultado=$querry->get_result();
+						if($resultado->num_rows === 0){ 
+							echo "";
+						}else{
+							while ($linha=$resultado->fetch_assoc()) {
+								?>
+									<?php echo($linha['cargo']); ?><input type="checkbox" id="<?php echo($linha['id_cargo']); ?>" name="cargo[]">
+								<?php
+							}	
+						}
+					?>
+				</label><br>
 				<label>Salario:</label>
 					<input name="salario">€<br>
 				<label>Nome:</label>
@@ -78,7 +189,7 @@
 				<label>Data de nascimento:</label>
 					<input type="date" name="dt_nasc"><br>
 				<label>Registo criminal:</label>
-					<input type="file" name="registo_criminal"><br>
+					<input type="file" name="registo_criminal" accept=".pdf,.doc"><br>
 				<label>Certificado academico:</label>
 					<input type="file" name="certificado_academico"><br>
 				<label>Certificado sbv/dae:</label>
